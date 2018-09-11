11ff87e102adcaf205d110c4506bd5a4dbf84729
Bogdan <bogdan@solarpulse.com.ua>
2018-09-11 22:27:30 +0200

    [issue] Fixed subnet ids - invalid number for index got -1 error

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "openvpn" {
   instance_type               = "${var.instance_type}"
   key_name                    = "${var.keyname}"
   source_dest_check           = false
-  subnet_id                   = "${element(var.subnet_ids, count.index - 1)}"
+  subnet_id                   = "${element(var.subnet_ids, count.index)}"
   user_data                   = "${element(data.template_file.openvpn.*.rendered, count.index)}"
   vpc_security_group_ids      = ["${aws_security_group.openvpn.id}","${aws_security_group.openvpn_public.id}","${var.extra_security_group_id}"]
   root_block_device {
